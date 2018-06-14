c851d76b48d7ab87b2ca447befffc7a2e6d9c49c
Jonathan Hong <jonhong777@gmail.com>
2018-06-14 13:03:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,12 @@
-/*var chocolateBars = ["snickers", "hundred", "grand", "kitkat", "skittles"]
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, newElement){
   var newArray = [newElement, ...array]
-  return array
+  return newArray
 }
 
 function destructivelyAddElementToBeginningOfArray(array, newElement){
   array.unshift(newElement);
   return array
 }
-*/
+
