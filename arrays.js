0e98cc6e52cc30cff3aaaccf941564abf4c5ecd5
Jonathan Hong <jonhong777@gmail.com>
2018-06-14 13:09:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,13 @@ function destructivelyAddElementToBeginningOfArray(array, newElement){
   array.unshift(newElement);
   return array
 }
+
+function addElementToEndOfArray(array, newElement){
+  var newArray = [...array, newElement]
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, newElement){
+  array.push(newElement)
+  return array
+}
