03b1a2625565262e3d480e106a9b8026aa8e5ff0
Jonathan Hong <jonhong777@gmail.com>
2018-06-14 13:29:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,3 +33,13 @@ function removeElementFromBeginningOfArray(array){
   var newArray = array.slice(1)
   return newArray
 }
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  var newArray = array.slice(0,array.length - 1)
+  return newArray
+}
